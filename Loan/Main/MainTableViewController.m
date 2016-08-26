c8cf04896b0e303db0aca139ec5545bcb2fc6b61
LM <287846224@qq.com>
2016-08-26 18:26:45 +0800

    界面微调。

--- Loan/Main/MainTableViewController.m
+++ Loan/Main/MainTableViewController.m
@@ -51,7 +51,7 @@
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.row < 3 || indexPath.row == 5 || indexPath.row == 7 || indexPath.row == 9 || indexPath.row == 11) {
+    if (indexPath.row < 3 || indexPath.row == 5 || indexPath.row == 7 || indexPath.row == 11) {
         return 56.0;
     }
     return 88.0;
